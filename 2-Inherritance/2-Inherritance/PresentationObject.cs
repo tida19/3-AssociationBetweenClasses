adef48c946c49134ba8bcc5dabe7364568498cdd
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-15 17:05:48 +0700

    2-Inherritance

---
+++ 2-Inherritance/2-Inherritance/PresentationObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2_Inherritance
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied clipboard");
+        }
+        public void Duplicate()
+        {
+            Console.WriteLine("Oject was duplicated");
+        }
+    }
+}
