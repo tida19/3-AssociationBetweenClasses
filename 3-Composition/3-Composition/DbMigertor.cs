4e065a51ce828cbaad30b19d6f1fafd5467a8f7f
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-15 17:21:19 +0700

    3-Composition

---
+++ 3-Composition/3-Composition/DbMigertor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Composition
+{
+    public class DbMigertor
+    {
+        private readonly Logger _logger;
+        public DbMigertor(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.Log("We are migrating blah blah blah...");
+
+        }
+    }
+}
---
+++ 3-Composition/3-Composition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigertor = new DbMigertor(new Logger());
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigertor.Migrate();
+            installer.Install();
+            Console.ReadKey();
+        }
+    }
+}
